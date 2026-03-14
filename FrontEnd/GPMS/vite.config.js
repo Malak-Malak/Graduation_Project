57bf17165f57591a3684ae3c052b04c37bccf35e
hananawad004 <hananawad004@gmail.com>
2026-03-15 01:25:41 +0200

    edit vite config for testing

--- FrontEnd/GPMS/vite.config.js
+++ FrontEnd/GPMS/vite.config.js
@@ -5,23 +5,44 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+
+//   server: {
+//     port: 5173,
+
+//     // ✅ Proxy — كل request يبدأ بـ /api بيروح للـ backend مباشرة
+//     // هذا يحل مشكلة CORS في التطوير بدون ما تحتاجي تعدلي الـ backend
+//     proxy: {
+//       '/api': {
+//         target: 'https://gpms.up.railway.app',
+//         changeOrigin: true,   // ← هذا هو اللي بيحل الـ CORS
+//         secure: true,
+//         // rewrite: (path) => path  // المسار بيبقى كما هو /api/...
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
 
   server: {
     port: 5173,
-
-    // ✅ Proxy — كل request يبدأ بـ /api بيروح للـ backend مباشرة
-    // هذا يحل مشكلة CORS في التطوير بدون ما تحتاجي تعدلي الـ backend
     proxy: {
       '/api': {
         target: 'https://gpms.up.railway.app',
-        changeOrigin: true,   // ← هذا هو اللي بيحل الـ CORS
+        changeOrigin: true,
         secure: true,
-        // rewrite: (path) => path  // المسار بيبقى كما هو /api/...
       }
     }
   }

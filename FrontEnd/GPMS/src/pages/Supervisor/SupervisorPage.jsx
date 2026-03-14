c9b14e44e16d834f3a882264cb9d37729eccf99b
hananawad004 <hananawad004@gmail.com>
2026-03-15 01:34:55 +0200

    fix some errors for deployment

--- FrontEnd/GPMS/src/pages/Supervisor/SupervisorPage.jsx
+++ FrontEnd/GPMS/src/pages/Supervisor/SupervisorPage.jsx
@@ -3,7 +3,7 @@ import MainLayout from "../../layout/MainLayout";
 
 import SupervisorDashboard from "../../components/common/supervisor/Dashboard/SupervisorDashboard";
 import GroupsList from "../../components/common/supervisor/Groups/GroupsList";
-import PendingRequests from "../../components/common/supervisor/PendingRequests/PendingRequests";
+import PendingRequests from "../../components/common/supervisor/Pendingrequests/Pendingrequests";
 import FileReview from "../../components/common/supervisor/FileReview/FileReview";
 import SupervisorMeetings from "../../components/common/supervisor/Meetings/SupervisorMeetings";
 import AIReports from "../../components/common/supervisor/AIReports/AIReports";

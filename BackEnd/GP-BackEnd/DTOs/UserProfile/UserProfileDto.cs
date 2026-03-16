8720e6ce965be2e09284386b48e8fda6f1dd54b4
Aya Abu Ghoza <ghozaaya@gmail.com>
2026-03-16 22:56:22 +0200

    added new apis for the User Profile

---
+++ BackEnd/GP-BackEnd/DTOs/UserProfile/UserProfileDto.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace GP_BackEnd.DTOs
+{
+    public class UserProfileDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public string? Field { get; set; }
+        public int TotalNumOfCreditCards { get; set; }
+        public bool IsGraduate { get; set; }
+        public int? MaxTeams { get; set; }
+    }
+}
+
